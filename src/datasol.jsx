46cfd37ec7f7f5549e901207f50eda1293b502dd
Zubin Madon <35947376+zubin-madon@users.noreply.github.com>
2022-02-11 08:53:55 +0530

    Update datasol.jsx
    Added projects

--- src/datasol.jsx
+++ src/datasol.jsx
@@ -8,6 +8,24 @@ const solProjects = [
     twitter: 'https://twitter.com/GeckosAI',
     projectLink: 'https://geckosAI.online', 
   },
+  {
+    id: 2,
+    name: 'Neural Apes Yacht Club',
+    description: '2222 Neural Apes on Solana.',
+    image:'https://www.nayc.online/logo192.png',
+    mintDate: 'Minting Now',
+    twitter: 'https://twitter.com/neuralapesYC',
+    projectLink: 'https://nayc.online', 
+  },
+  {
+    id: 3,
+    name: 'PottyPunks',
+    description: '4444 of the OG 10k Punks on the Pot.',
+    image:'https://testlaunchmynft.mypinata.cloud/ipfs/QmRroiapUeXaf8Ubw61QyfLRkCkP2V6GSsNr3CNXNHa5UP/17.png',
+    mintDate: 'Minting Now',
+    twitter: 'https://twitter.com/PottyPunks',
+    projectLink: 'https://launchmynft.io/collections/ALMHvvutxdaCFU24nL1sohwegimZCknVvsKF6L2JLP6f/lOkMIuFKDo46gddEn5lV', 
+  }
 ];
 
 export default solProjects;
